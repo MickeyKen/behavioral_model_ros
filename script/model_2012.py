f32c47107ad0e22b720a758e5345cd2c6436099c
MickeyKen <mickey333ff@gmail.com>
2019-04-20 02:48:33 +0900

    [Edit] subscriber

--- script/model_2012.py
+++ script/model_2012.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
-import rospy
+import rospy, actionlib, math, tf
+from actionlib_msgs.msg import *
 import smach
 import smach_ros
 import random
-from smach_ros import ServiceState
-from smach_ros import SimpleActionState
+from smach_ros import ServiceState, SimpleActionState
+from geometry_msgs import Pose, Quaternion
+from move_base_msgs import MoveBaseAction, MoveBaseGoal
 
 class Search_and_Wander(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['to_A'])
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing int state ISTATE')
-        rospy.sleep(1)
-        return 'to_A'
+        self.subscriber = rospy.Subscriber('/test', Pose, self.callback)
 
+    def callback(self, data):
+        rospy.set_param('/target_human_pose/x', data.x)
+        rospy.set_param('/target_human_pose/y', data.y)
+        return data.data
+
+def calc_pose():
+    ### get human position ###
+    x_value = rospy.get_param("/target_human_pose/x")
+    y_value = rospy.get_param("/target_human_pose/y")
 
 class Approach(smach.State):
     def __init__(self):
@@ -28,7 +36,6 @@ class Approach(smach.State):
             return 'to_SW'
 
         return 'to_P'
-
 
 class Provide(smach.State):
     def __init__(self):
@@ -56,8 +63,28 @@ if __name__ == '__main__':
         ###start Search_and_Wander ###
         sw_sub = smach.StateMachine(outcomes=['success'])
         with sw_sub:
+            ### init position ###
+            pose  = MoveBaseGoal()
+            pose.target_pose.header.frame_id = "base_link"
+            pose.target_pose.header.stamp = rospy.Time.now()
+            pose.target_pose.pose.position.x =  0.0
+            pose.target_pose.pose.position.y =  1.0
+            q = tf.transformations.quaternion_from_euler(0, 0, 3.14)
+            pose.target_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
+            smach.StateMachine.add('Set_Pose',
+                                   SimpleActionState('/move_base/goal',
+                                                     MoveBaseAction,
+                                                     goal=pose),
+                                   transitions={'succeeded': 'Search_and_Wander',
+                                                'preempted': 'Set_Pose',
+                                                'aborted': 'Set_Pose'})
+
             smach.StateMachine.add('Search_and_Wander', Search_and_Wander(),
                                transitions={'to_A':'Approach'})
+
+            smach.StateMachine.add('Calcurate_Pose',
+                                   CBState(calc_pose),
+                                   {'success': 'success'})
 
 
         smach.StateMachine.add('Search_and_Wander', sw_sub,
