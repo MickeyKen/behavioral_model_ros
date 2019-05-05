21580ce0eb45bbcb0f0faf075fb579a3f771e8c2
MickeyKen <mickey333ff@gmail.com>
2019-05-06 00:24:42 +0900

    [Add] rviz publiser

--- script/prediction_human.py
+++ script/prediction_human.py
@@ -42,36 +42,38 @@ class Publishsers():
         # print self.x
         for n in range(len(self.measurements)):
 
-            F[0][2] = self.dtArr[n]
-            F[1][3] = self.dtArr[n]
+            if self.dtArr[n] > 0.03 and self.dtArr[n] < 0.045:
+                F[0][2] = self.dtArr[n]
+                F[1][3] = self.dtArr[n]
+                # print self.dtArr[n]
 
-            # prediction
-            # self.x = (F * self.x) + u
-            self.x = np.dot(F, self.x) + u
-            # P = F * P * F.T
-            P = np.dot(np.dot(F, P), F.T)
+                # prediction
+                # self.x = (F * self.x) + u
+                self.x = np.dot(F, self.x) + u
+                # P = F * P * F.T
+                P = np.dot(np.dot(F, P), F.T)
 
 
 
-            # measurement update
-            Z = np.array([self.measurements[n]])
-            # y = Z.T - (H * self.x)
-            y = Z.T - np.dot(H, self.x)
+                # measurement update
+                Z = np.array([self.measurements[n]])
+                # y = Z.T - (H * self.x)
+                y = Z.T - np.dot(H, self.x)
 
-            # S = H * P * H.T + R
-            S = np.dot(np.dot(H, P), H.T) + R
+                # S = H * P * H.T + R
+                S = np.dot(np.dot(H, P), H.T) + R
 
-            # K = P * H.T * np.linalg.inv(S)
-            K = np.dot(np.dot(P, H.T), np.linalg.inv(S))
+                # K = P * H.T * np.linalg.inv(S)
+                K = np.dot(np.dot(P, H.T), np.linalg.inv(S))
 
-            # self.x = self.x + (K * y)
-            self.x = self.x + np.dot(K, y)
+                # self.x = self.x + (K * y)
+                self.x = self.x + np.dot(K, y)
 
-            # P = (I - (K * H)) * P
-            P = np.dot((I - np.dot(K, H)), P)
+                # P = (I - (K * H)) * P
+                P = np.dot((I - np.dot(K, H)), P)
 
-        print self.x[0][0]
-        
+            # print self.x[0][0]
+
         return self.x, P
 
 
@@ -136,8 +138,9 @@ class Server(Publishsers):
                     if self.x[0][0] == 0.0 and self.x[1][0] == 0.0:
                         self.x[0][0] = i.pos.x
                         self.x[1][0] = i.pos.y
+                        self.now = rospy.get_time()
                     else:
-                        print "pass"
+                        # print "pass"
                         self.now = rospy.get_time()
                         dt = self.now - self.past
                         self.measurements = np.append(self.measurements, [[i.pos.x,i.pos.y]], axis=0)
@@ -162,17 +165,14 @@ class Server(Publishsers):
         rospy.sleep(1.0)
 
         x, P = self.filter()
-        print x
-        print P
-
 
         # init PoseArray() and create and publish
-        # self.prediction_msg = PoseArray()
-        # for i in pose:
-        #     self.prediction_make(pose[i]][0],pose[i][1])
-        # self.prediction_msg.header.stamp = rospy.Time.now()
-        # self.prediction_msg.header.frame_id = "/base_scan"
-        # self.prediction_pub.publish(self.prediction_msg)
+        self.prediction_msg = PoseArray()
+        for i in range(5):
+            self.prediction_make(x[0][0] + x[2][0] * (i + 1), x[1][0] + x[3][0] * (i + 1))
+        self.prediction_msg.header.stamp = rospy.Time.now()
+        self.prediction_msg.header.frame_id = "/base_scan"
+        self.prediction_pub.publish(self.prediction_msg)
 
 
         return_string.data = "true"
