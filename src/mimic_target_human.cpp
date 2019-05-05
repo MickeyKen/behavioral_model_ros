017b4be2d598f61de473144d73db061dc752a589
MickeyKen <mickey333ff@gmail.com>
2019-05-06 03:44:00 +0900

    [Add] target_human_head

--- src/mimic_target_human.cpp
+++ src/mimic_target_human.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 #include <geometry_msgs/Pose.h>
 
 class Mimic
@@ -17,14 +18,15 @@ Mimic::Mimic()
 {
   ros::NodeHandle input_nh("input");
   ros::NodeHandle output_nh("output");
-  marker_pub_ = output_nh.advertise<visualization_msgs::Marker> ("/target_human/marker", 1);
-  pose_sub_ = input_nh.subscribe<geometry_msgs::Pose> ("/target_human/pose", 1, &Mimic::poseCallback, this);
+  marker_pub_ = output_nh.advertise<visualization_msgs::MarkerArray> ("/target_human/marker", 10);
+  pose_sub_ = input_nh.subscribe<geometry_msgs::Pose> ("/target_human/pose", 10, &Mimic::poseCallback, this);
 }
 
 void Mimic::poseCallback(const geometry_msgs::PoseConstPtr& pose)
 {
+  visualization_msgs::MarkerArray markers;
   visualization_msgs::Marker marker_body;
-  // visualization_msgs::Marker marker_head;
+  visualization_msgs::Marker marker_head;
 
   marker_body.header.frame_id = "/base_scan";
   marker_body.header.stamp = ros::Time::now();
@@ -53,8 +55,38 @@ void Mimic::poseCallback(const geometry_msgs::PoseConstPtr& pose)
   marker_body.color.a = 1.0;
 
   marker_body.lifetime = ros::Duration(0.5);
+  markers.markers.push_back(marker_body);
 
-  marker_pub_.publish(marker_body);
+  marker_head.header.frame_id = "/base_scan";
+  marker_head.header.stamp = ros::Time::now();
+
+  marker_head.ns = "basic_shapes";
+  marker_head.id = 1;
+  marker_head.type = 2;
+  marker_head.action = visualization_msgs::Marker::ADD;
+  marker_head.pose.position.x = pose->position.x;
+  marker_head.pose.position.y = pose->position.y;
+  marker_head.pose.position.z = 1.95;
+  marker_head.pose.orientation.x = 0.0;
+  marker_head.pose.orientation.y = 0.0;
+  marker_head.pose.orientation.z = 0.0;
+  marker_head.pose.orientation.w = 1.0;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker_head.scale.x = 0.5;
+  marker_head.scale.y = 0.5;
+  marker_head.scale.z = 0.5;
+
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker_head.color.r = 1.0f;
+  marker_head.color.g = 0.0f;
+  marker_head.color.b = 0.0f;
+  marker_head.color.a = 1.0;
+
+  marker_head.lifetime = ros::Duration(0.5);
+  markers.markers.push_back(marker_head);
+
+  marker_pub_.publish(markers);
 }
 int main(int argc, char** argv)
 {
